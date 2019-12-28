e40fd29c20b33ac45f32fb9cc7a78313d688b8c6
hhwf <329589951@qq.com>
2019-12-28 09:37:40 +0800

    Initial commit

---
+++ src/main/java/com/hhwf/leetcode/AddTwoNumbers.java
@@ -0,0 +1,59 @@
+package com.hhwf.leetcode;
+
+/**
+ * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+ * <p>
+ * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+ * <p>
+ * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ * <p>
+ * 示例：
+ * <p>
+ * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * 输出：7 -> 0 -> 8
+ * 原因：342 + 465 = 807
+ * <p>
+ */
+public class AddTwoNumbers {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) return null;
+        int sum = 0;
+        ListNode res = new ListNode(0);
+        ListNode curr = res;
+        while (l1 != null || l2 != null) {
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            int val = sum % 10;
+            sum = sum / 10;
+            ListNode node = new ListNode(val);
+            res.next = node;
+            res = res.next;
+        }
+        //处理sum进位
+        if (sum != 0) {
+            ListNode next = new ListNode(sum);
+            res.next = next;
+        }
+
+        return curr.next;
+    }
+
+
+}
---
+++ src/main/java/com/hhwf/leetcode/LengthOfLongestSubstring.java
@@ -0,0 +1,59 @@
+package com.hhwf.leetcode;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+ * 示例 1:
+ * <p>
+ * 输入: "abcabcbb"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ * 示例 2:
+ * <p>
+ * 输入: "bbbbb"
+ * 输出: 1
+ * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+ * 示例 3:
+ * <p>
+ * 输入: "pwwkew"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+ *      请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+ */
+public class LengthOfLongestSubstring {
+
+
+    /**
+     * @param s
+     * @return
+     */
+    public static int lengthOfLongestSubstring(String s) {
+        /**
+         * res 返回值
+         * 利用set类型存放连续不重复数据 ，k表示不重复数据开始地方 ，特别注意当set中包含时 i不变
+         */
+        if (s.length() == 0) return 0;
+        int res = 0;
+        Set<Character> set = new HashSet<Character>();
+        int k = 0;
+        for (int i = 0; i < s.length(); ) {
+            if (set.contains(s.charAt(i))) {
+                set.remove(s.charAt(k++));
+            } else {
+                set.add(s.charAt(i++));
+                res = Math.max(res,set.size());
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("qrsvbspk"));
+    }
+
+}
+
